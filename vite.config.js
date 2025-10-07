67cf0ffe3f2f85a924815a410783708de09c26e0
rob522 <rob@out.land>
2025-10-07 15:38:39 +0100

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/my-3d-viewer/", // This ensures your site works on GitHub Pages
+  base: "/my-3d-viewer/",
   plugins: [react()],
 });
