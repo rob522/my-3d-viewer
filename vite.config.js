b21ea7d5bf2c8cd3a914b4d32afc212cd46e1c86
rob522 <rob@out.land>
2025-10-07 15:40:16 +0100

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/my-3d-viewer/",
   plugins: [react()],
+  base: "/my-3d-viewer/"
 });
